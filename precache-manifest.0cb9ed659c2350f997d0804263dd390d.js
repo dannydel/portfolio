6e502aa767de18f7eb3620427ff93ce1733f5b50
Danny Del Grosso <dannydelgrosso@gmail.com>
2020-07-28 20:17:10 -0400

    Updates

---
+++ precache-manifest.0cb9ed659c2350f997d0804263dd390d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94aa2e167d51e6448365567666fb15d5",
+    "url": "/dannydel/portfolio/index.html"
+  },
+  {
+    "revision": "13be8dc0208f6b4648ef",
+    "url": "/dannydel/portfolio/static/js/2.c35d4a7b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dannydel/portfolio/static/js/2.c35d4a7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79c26b02a64ee2a490e6",
+    "url": "/dannydel/portfolio/static/js/main.d1415e50.chunk.js"
+  },
+  {
+    "revision": "0e892e30fe6156a18bd2",
+    "url": "/dannydel/portfolio/static/js/runtime-main.fa465941.js"
+  }
+]);
